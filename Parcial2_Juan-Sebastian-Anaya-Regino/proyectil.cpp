c06b2d454c4fab2b1e928966736cdfe3d028ffa5
Juan Anaya <juan.anayar@udea.edu.co>
2020-12-14 06:37:47 -0500

    Agregue las funciones set y get de los datos miembros.

--- Parcial2_Juan-Sebastian-Anaya-Regino/proyectil.cpp
+++ Parcial2_Juan-Sebastian-Anaya-Regino/proyectil.cpp
@@ -1,6 +1,38 @@
 #include "proyectil.h"
 
-Proyectil::Proyectil()
+float Proyectil::getRSensor() const
 {
+    return rSensor;
+}
 
+Proyectil::Proyectil(float X0, float Y0)
+{
+    X = X0;
+    Y = Y0;
+}
+
+float Proyectil::getX() const
+{
+    return X;
+}
+
+float Proyectil::getY() const
+{
+    return Y;
+}
+
+void Proyectil::setX(float Xnew)
+{
+    X = Xnew;
+}
+
+void Proyectil::setY(float Ynew)
+{
+    Y = Ynew;
+}
+
+
+void Proyectil::setRSensor(float rango)
+{
+    rSensor = rango;
 }
--- Parcial2_Juan-Sebastian-Anaya-Regino/proyectil.h
+++ Parcial2_Juan-Sebastian-Anaya-Regino/proyectil.h
@@ -1,11 +1,23 @@
 #ifndef PROYECTIL_H
 #define PROYECTIL_H
 
-
 class Proyectil
 {
+private:
+    float X; //Posicion en X
+    float Y; //Posicion en Y
+    float rSensor; //Rango del sensor
+
 public:
-    Proyectil();
+    Proyectil(float X0, float Y0);
+
+    void setX(float Xnew);
+    void setY(float Ynew);
+    void setRSensor(float rango);
+
+    float getX() const;
+    float getY() const;
+    float getRSensor() const;
 };
 
 #endif // PROYECTIL_H
