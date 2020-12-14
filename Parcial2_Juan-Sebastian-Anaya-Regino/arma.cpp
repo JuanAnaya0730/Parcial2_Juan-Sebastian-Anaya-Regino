77946f2e6dc64afea152583785b2cc224ad05c65
Juan Anaya <juan.anayar@udea.edu.co>
2020-12-14 06:36:50 -0500

    Agregue una la funcion de disparar.

--- Parcial2_Juan-Sebastian-Anaya-Regino/arma.cpp
+++ Parcial2_Juan-Sebastian-Anaya-Regino/arma.cpp
@@ -1,6 +1,45 @@
 #include "arma.h"
 
-Arma::Arma()
+Arma::Arma(float X0, float Y0)
 {
+    X = X0;
+    Y = Y0;
+}
 
+//void Arma::apunta(Arma objetivo)
+//{
+//    Xf = objetivo.getX();
+//    Yf = objetivo.getY();
+//}
+
+int Arma::disparar(Arma objetivo, float V0, int angulo)
+{
+    Proyectil bala(X, Y);
+    bala.setRSensor(0.05*objetivo.getX());
+    float Vx, Vy;
+
+    Vx = V0*cos((angulo*pi)/180);
+
+    for(float t=0; sqrt((pow((objetivo.getX()-bala.getX()), 2)+(pow((objetivo.getY()-bala.getY()), 2)))) > bala.getRSensor(); t+=0.1){
+        Vy = V0*sin((angulo*pi)/180);
+
+        bala.setX(X + Vx*t);
+        bala.setY(Y + Vy*t - ((g*t*t)/2));
+
+        if(bala.getY() <=0){
+            cout << "no llego" << endl;
+            return 0;
+        }
+    }
+    return 1;
+}
+
+float Arma::getX() const
+{
+    return X;
+}
+
+float Arma::getY() const
+{
+    return Y;
 }
--- Parcial2_Juan-Sebastian-Anaya-Regino/arma.h
+++ Parcial2_Juan-Sebastian-Anaya-Regino/arma.h
@@ -1,11 +1,31 @@
 #ifndef ARMA_H
 #define ARMA_H
 
+#include <iostream>
+#include <math.h>
+#include "proyectil.h"
+
+using namespace std;
+
+#define g 9.81
+#define pi 3.14159
 
 class Arma
 {
+private:
+    float X; //Posicion en X del arma.
+    float Y; //Posicion en Y del arma.
+
+//    float Xf; //Posicion final en X de un disparo.
+//    float Yf; //Posicion final en Y de un disparo.
+
 public:
-    Arma();
+    Arma(float X0, float Y0);
+    //void apunta(Arma objetivo);
+    int disparar(Arma objetivo, float V0, int angulo);
+
+    float getX() const;
+    float getY() const;
 };
 
 #endif // ARMA_H
